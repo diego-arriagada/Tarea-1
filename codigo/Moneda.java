77dd8a0cf3805fd4e65ada226f2e9f89c6a9e637
Diego Arriagada <diarriagada2023@udec.cl>
2025-05-01 23:02:30 -0400

    Moneda implementa comparable y se le agrega metodo compareTo vacio provisional

--- codigo/Moneda.java
+++ codigo/Moneda.java
@@ -1,4 +1,4 @@
-abstract class Moneda { //
+abstract class Moneda implements Comparable { //
     public Moneda() {
     }
     public Moneda getSerie() { // retorna la referencia a la moneda
@@ -6,4 +6,8 @@ abstract class Moneda { //
     }
     public abstract int getValor(); // retorna el valor de la moneda
 
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
 }
