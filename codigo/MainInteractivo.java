a6bde81b12652312ce0c75304f9ef47edf62752a
unknown <mcatril2018@udec.cl>
2025-05-02 23:30:35 -0400

    Actualización info para javadoc de Main Interactivo

--- codigo/MainInteractivo.java
+++ codigo/MainInteractivo.java
@@ -1,10 +1,11 @@
 /**
  * La clase `MainInteractivo` es una clase donde se puede utilizar
- * las funciones de la máquina expendedora sin limitaciones, hasta que el
+ * las funciones de la máquina expendedora casi sin limitaciones, hasta que el
  * usuario decida detenerse
  *
- * Contiene múltiples depósitos de productos y un depósito para monedas devueltas.
- * La clase permite comprar productos y calcular el vuelto.
+ * La unica limitación de este main interactivo son los productos disponibles dentro
+ * de la maquina expendedora, que se limita a 10 productos por tipo de producto para poder
+ * testear también que sucede en caso de que se acabe el stock de algún deposito
  *
  * @author Diego Arriagada
  * @author Victor Galaz
